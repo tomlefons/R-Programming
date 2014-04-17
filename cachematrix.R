33fd9657aae3f467a40ec289f11ad9bce2be04a9
tomlefons <tomlefons@googlemail.com>
2014-04-17 11:07:22 +0200

    1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
+## Matrix inversion is usually a costly computation and 
+## their may be some benefit to caching the inverse of a matrix 
+## rather than compute it repeatedly 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {    
+    inv <- NULL                                                       
+    set <- function(y) {                                                # set function defintion
+        x <<- y                                                         # variable x to y
+        inv <- NULL                                                     # if change, flush cache
+    }
+    get <- function() x                                                 # get function definiton
+    getSolve <- function() inv                                          # receive the inversed cached value 
+    setSolve <- function(computed_inv) inv <<- computed_inv             # transfer values into the cache  
+    list(get=get, set=set, getSolve=getSolve, setSolve=setSolve)        # list is returned
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated and the matrix has not changed, 
+## then the cachesolve retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getSolve() 
+    if(!is.null(inv)) {                                                 # entry available in cache
+        message("getting cached data")
+        return(inv)
+    }
+    new_inv <- solve(x$get(), ...)                                      # inverse calculation   
+    x$setSolve(new_inv)                                                 # cache load with inverse      
+    return(new_inv)
 }
